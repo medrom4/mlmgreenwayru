f4db6940cec2751aadbdbe2cec62181719beb1a2
medrom4 <medikroma@gmail.com>
2019-05-22 17:39:51 +0300

    html

--- mlmgreenwayRU/wp-content/themes/mlmgreenwayru/footer.php
+++ mlmgreenwayRU/wp-content/themes/mlmgreenwayru/footer.php
@@ -39,20 +39,6 @@
 <?php wp_footer(); ?>
 
 
-<script type="text/javascript">
-	$(document).ready(function() {
-		$(".menu").on("click", "a", function(event) {
-			event.preventDefault();
-			var id = $(this).attr('href'),
-				top = $(id).offset().top;
-			$('body,html').animate({
-				scrollTop: top
-			}, 1500);
-		});
-	});
-</script>
-
-
 </body>
 <!-- END BODY -->
 
